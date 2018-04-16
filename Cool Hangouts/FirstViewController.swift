fb6b1a096b6f6e0a18a2c1766e1ae4c073853f5c
Kyle Kamibayashi <kylekamibayashi@claires-macbook-pro.local>
2018-04-16 16:28:25 -0700

    Initial Commit

---
+++ Cool Hangouts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cool Hangouts
+//
+//  Created by Kyle Kamibayashi on 4/16/18.
+//  Copyright © 2018 Kyle Kamibayashi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
